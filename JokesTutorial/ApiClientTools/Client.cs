3a55dbfd0f3864575794a8b7aa45f3f289cbdb01
Mircea Nicolae <cristian@weanswer.it>
2021-03-11 13:33:54 +0200

    separate class

---
+++ JokesTutorial/ApiClientTools/Client.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Net.Http;
+using JokesTutorial.Data;
+using JokesTutorial.Models;
+using JokesTutorial.Libraries;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Dynamic;
+using System.Web;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ApiClientTools
+{
+    public class Client
+    {
+        public static string getApiBaseUrl()
+        {
+            var apiBaseUrl = Environment.GetEnvironmentVariable("API_CLIENT_BASE_URL").TrimEnd(new char[] {'/'});
+    
+            return apiBaseUrl;
+        }
+
+        public static string buildUrl(string endpoint, Dictionary<string, string> endpointParams, Dictionary<string, string> endpointData)
+        {
+            var endpointUrl = endpoint;
+
+            foreach (Match match in Regex.Matches(endpoint, @"{(.*?)}")) {
+                var matchedParam = match.Value.TrimStart('{').TrimEnd('}');
+                if(endpointParams.ContainsKey(matchedParam)) {
+                    endpointUrl = endpointUrl.Replace(match.Value, HttpUtility.UrlEncode(endpointParams[matchedParam]));
+                }
+            }
+
+            if(endpointData.Count>0) {
+                List<string> dataParts = new List<string>();
+                foreach(KeyValuePair<string, string> part in endpointData) {
+                    dataParts.Add(HttpUtility.UrlEncode(part.Key)+"="+HttpUtility.UrlEncode(part.Value));
+                }
+                endpointUrl += "?"+String.Join("&", dataParts.ToArray());
+            }
+            
+            return getApiBaseUrl() + '/' + endpointUrl.TrimStart(new char[] {'/'});
+        }
+
+        public static HttpRequestMessage prepareRequest(HttpMethod method, string url)
+        {
+            HttpRequestMessage request;
+            string apiKey;
+
+            request = new HttpRequestMessage(method, url);
+            apiKey = Environment.GetEnvironmentVariable("API_CLIENT_SECRET");
+
+            request.Headers.Add("Accept", "application/json");
+            request.Headers.Add("X-Api-Key", apiKey);
+
+            return request;
+        }
+
+        public static async Task<ExpandoObject> doGet(string endpoint, Dictionary<string, string> endpointParams, Dictionary<string, string> endpointData)
+        {
+            //apiDebug = Convert.ToBoolean(Environment.GetEnvironmentVariable("API_CLIENT_DEBUG"));
+
+            string endpointUrl;
+            HttpRequestMessage request;
+            HttpClient client;
+
+            endpointUrl = buildUrl(endpoint, endpointParams, endpointData);
+            request = prepareRequest(HttpMethod.Get, endpointUrl);
+            client = new HttpClient();
+
+            var response = await client.SendAsync(request);
+
+            
+            var stringResponse = await response.Content.ReadAsStringAsync();
+            return processResponse(HttpMethod.Get, endpointUrl, stringResponse, response.IsSuccessStatusCode);
+        }
+
+        public static async Task<ExpandoObject> doPost(string endpoint, Dictionary<string, string> endpointParams, ExpandoObject data)
+        {
+            //apiDebug = Convert.ToBoolean(Environment.GetEnvironmentVariable("API_CLIENT_DEBUG"));
+
+            string endpointUrl;
+            HttpRequestMessage request;
+            HttpClient client;
+
+            endpointUrl = buildUrl(endpoint, endpointParams, new Dictionary<string, string> {});
+            request = prepareRequest(HttpMethod.Post, endpointUrl);
+
+            var jsonPayload = JsonConvert.SerializeObject(data);
+            var requestContent = new StringContent(jsonPayload, System.Text.Encoding.UTF8, "application/json");
+
+            request.Content = requestContent;
+
+            client = new HttpClient();
+
+            var response = await client.SendAsync(request);
+
+            
+            var stringResponse = await response.Content.ReadAsStringAsync();
+            return processResponse(HttpMethod.Post, endpointUrl, stringResponse, response.IsSuccessStatusCode);
+        }
+
+        public static ExpandoObject processResponse(HttpMethod method, string endpointUrl, string stringResponse, bool isSuccessful)
+        {
+            dynamic jsonResponse;
+            dynamic SuccessfulResponse;
+
+            if(method == HttpMethod.Get) {
+                SuccessfulResponse = new { success = false, data = new ExpandoObject() };
+            } else {
+                SuccessfulResponse = new { success = false, data = new ExpandoObject(), ack = false };
+            }
+            
+            
+            var UnsuccessfulResponse = new { success = false, data = new ExpandoObject(), message="" };
+
+
+            if(isSuccessful)
+            {
+                try {
+                    jsonResponse = JsonConvert.DeserializeAnonymousType(stringResponse, SuccessfulResponse);
+                } catch (JsonReaderException ex) {
+                    throw new ArgumentException("There was a problem parsing the JSON", stringResponse, ex);
+                }
+                
+            } else {
+                jsonResponse = JsonConvert.DeserializeAnonymousType(stringResponse, UnsuccessfulResponse);
+                throw new ArgumentException(jsonResponse.message, stringResponse);
+            }
+
+            return jsonResponse.data;
+        }
+
+        public static ExpandoObject getFilePayloadFromPath(string filePath, string role = "files", int order = 0)
+        {
+            dynamic fileData;
+
+            if(!System.IO.File.Exists(filePath)) {
+                throw new ArgumentException("Couldn't find a file at path", filePath);
+            }
+
+            
+
+            fileData = new ExpandoObject();
+
+            FileInfo fileInfo;
+            string fileMime;
+
+            new Microsoft.AspNetCore.StaticFiles.FileExtensionContentTypeProvider().TryGetContentType(filePath, out fileMime);
+            fileInfo = new FileInfo(filePath);
+            
+
+            String filePayload = Convert.ToBase64String(System.IO.File.ReadAllBytes(filePath));
+
+            fileData.mime = fileMime;
+            fileData.name = Path.GetFileNameWithoutExtension(filePath);
+            fileData.extension = Path.GetExtension(filePath).TrimStart('.');
+
+            fileData.basename = fileInfo.Name;
+            fileData.size = fileInfo.Length;
+            fileData.lastModified = System.IO.File.GetLastWriteTimeUtc(filePath).ToString("yyyy-MM-dd HH:mm:ss");
+            fileData.originalPath = Path.GetFullPath(filePath);
+
+            fileData.role = role;
+            fileData.order = order;
+
+            fileData.content = filePayload;
+
+            return fileData;
+        }
+    }
+}
--- JokesTutorial/Controllers/JokesController.cs
+++ JokesTutorial/Controllers/JokesController.cs
@@ -40,166 +40,46 @@ namespace JokesTutorial.Controllers
             return View("SearchForm");
         }
 
-        public static string getApiBaseUrl()
+        // GET: Jokes/Policy
+        public IActionResult Policy()
         {
-            var apiBaseUrl = Environment.GetEnvironmentVariable("API_CLIENT_BASE_URL").TrimEnd(new char[] {'/'});
-    
-            return apiBaseUrl;
+            string apiEndPoint;
+            Dictionary<string, string> endpointParams;
+            dynamic endpointData;
+
+            apiEndPoint = "api/policies/attachFile/{id}";
+            endpointParams = new Dictionary<string, string>() {{ "id", "1" }};
+
+            endpointData = new ExpandoObject();
+            endpointData.file = ApiClientTools.Client.getFilePayloadFromPath("test.pdf");
+
+            var data = ApiClientTools.Client.doPost(apiEndPoint, endpointParams, endpointData);
+            return Json(data.Result);
         }
 
-        public static string buildUrl(string endpoint, Dictionary<string, string> endpointParams, Dictionary<string, string> endpointData)
+        // GET: Jokes/Will
+        public IActionResult Will()
         {
-            var endpointUrl = endpoint;
+            string apiEndPoint;
+            Dictionary<string, string> endpointParams;
+            dynamic endpointData;
 
-            foreach (Match match in Regex.Matches(endpoint, @"{(.*?)}")) {
-                var matchedParam = match.Value.TrimStart('{').TrimEnd('}');
-                if(endpointParams.ContainsKey(matchedParam)) {
-                    endpointUrl = endpointUrl.Replace(match.Value, HttpUtility.UrlEncode(endpointParams[matchedParam]));
-                }
-            }
+            apiEndPoint = "api/wills/attachFile/{id}";
+            endpointParams = new Dictionary<string, string>() {{ "id", "1" }};
 
-            if(endpointData.Count>0) {
-                List<string> dataParts = new List<string>();
-                foreach(KeyValuePair<string, string> part in endpointData) {
-                    dataParts.Add(HttpUtility.UrlEncode(part.Key)+"="+HttpUtility.UrlEncode(part.Value));
-                }
-                endpointUrl += "?"+String.Join("&", dataParts.ToArray());
-            }
-            
-            return getApiBaseUrl() + '/' + endpointUrl.TrimStart(new char[] {'/'});
+            endpointData = new ExpandoObject();
+            endpointData.file = ApiClientTools.Client.getFilePayloadFromPath("test.exe");
+
+            var data = ApiClientTools.Client.doPost(apiEndPoint, endpointParams, endpointData);
+            return Json(data.Result);
         }
-
-        public static HttpRequestMessage prepareRequest(HttpMethod method, string url)
-        {
-            HttpRequestMessage request;
-            string apiKey;
-
-            request = new HttpRequestMessage(method, url);
-            apiKey = Environment.GetEnvironmentVariable("API_CLIENT_SECRET");
-
-            request.Headers.Add("Accept", "application/json");
-            request.Headers.Add("X-Api-Key", apiKey);
-
-            return request;
-        }
-
-        public static async Task<ExpandoObject> doGet(string endpoint, Dictionary<string, string> endpointParams, Dictionary<string, string> endpointData)
-        {
-            //apiDebug = Convert.ToBoolean(Environment.GetEnvironmentVariable("API_CLIENT_DEBUG"));
-
-            string endpointUrl;
-            HttpRequestMessage request;
-            HttpClient client;
-
-            endpointUrl = buildUrl(endpoint, endpointParams, endpointData);
-            request = prepareRequest(HttpMethod.Get, endpointUrl);
-            client = new HttpClient();
-
-            var response = await client.SendAsync(request);
-
-            
-            var stringResponse = await response.Content.ReadAsStringAsync();
-            return processResponse(HttpMethod.Get, endpointUrl, stringResponse, response.IsSuccessStatusCode);
-        }
-
-        public static async Task<ExpandoObject> doPost(string endpoint, Dictionary<string, string> endpointParams, ExpandoObject data)
-        {
-            //apiDebug = Convert.ToBoolean(Environment.GetEnvironmentVariable("API_CLIENT_DEBUG"));
-
-            string endpointUrl;
-            HttpRequestMessage request;
-            HttpClient client;
-
-            endpointUrl = buildUrl(endpoint, endpointParams, new Dictionary<string, string> {});
-            request = prepareRequest(HttpMethod.Post, endpointUrl);
-
-            var jsonPayload = JsonConvert.SerializeObject(data);
-            var requestContent = new StringContent(jsonPayload, System.Text.Encoding.UTF8, "application/json");
-
-            request.Content = requestContent;
-
-            client = new HttpClient();
-
-            var response = await client.SendAsync(request);
-
-            
-            var stringResponse = await response.Content.ReadAsStringAsync();
-            return processResponse(HttpMethod.Post, endpointUrl, stringResponse, response.IsSuccessStatusCode);
-        }
-
-        public static ExpandoObject processResponse(HttpMethod method, string endpointUrl, string stringResponse, bool isSuccessful)
-        {
-            dynamic jsonResponse;
-            dynamic SuccessfulResponse;
-
-            if(method == HttpMethod.Get) {
-                SuccessfulResponse = new { success = false, data = new ExpandoObject() };
-            } else {
-                SuccessfulResponse = new { success = false, data = new ExpandoObject(), ack = false };
-            }
-            
-            
-            var UnsuccessfulResponse = new { success = false, data = new ExpandoObject(), message="" };
-
-
-            if(isSuccessful)
-            {
-                try {
-                    jsonResponse = JsonConvert.DeserializeAnonymousType(stringResponse, SuccessfulResponse);
-                } catch (JsonReaderException ex) {
-                    throw new ArgumentException("There was a problem parsing the JSON", stringResponse, ex);
-                }
-                
-            } else {
-                jsonResponse = JsonConvert.DeserializeAnonymousType(stringResponse, UnsuccessfulResponse);
-                throw new ArgumentException(jsonResponse.message, stringResponse);
-            }
-
-            return jsonResponse.data;
-        }
-
-        public static ExpandoObject getFilePayloadFromPath(string filePath, string role = "files", int order = 0)
-        {
-            dynamic fileData;
-
-            if(!System.IO.File.Exists(filePath)) {
-                throw new ArgumentException("Couldn't find a file at path", filePath);
-            }
-
-            
-
-            fileData = new ExpandoObject();
-
-            FileInfo fileInfo;
-            string fileMime;
-
-            new Microsoft.AspNetCore.StaticFiles.FileExtensionContentTypeProvider().TryGetContentType(filePath, out fileMime);
-            fileInfo = new FileInfo(filePath);
-            
-
-            String filePayload = Convert.ToBase64String(System.IO.File.ReadAllBytes(filePath));
-
-            fileData.mime = fileMime;
-            fileData.name = Path.GetFileNameWithoutExtension(filePath);
-            fileData.extension = Path.GetExtension(filePath).TrimStart('.');
-
-            fileData.basename = fileInfo.Name;
-            fileData.size = fileInfo.Length;
-            fileData.lastModified = System.IO.File.GetLastWriteTimeUtc(filePath).ToString("yyyy-MM-dd HH:mm:ss");
-            fileData.originalPath = Path.GetFullPath(filePath);
-
-            fileData.role = role;
-            fileData.order = order;
-
-            fileData.content = filePayload;
-
-            return fileData;
-        }
+        
 
         // GET: Jokes/Test
         public IActionResult Test()
         {
-            dynamic testPayload = getFilePayloadFromPath("test.pdf");
+            return Content("asdas");
+            dynamic testPayload = ApiClientTools.Client.getFilePayloadFromPath("test.pdf");
 
             string apiEndPoint;
             Dictionary<string, string> endpointParams;
@@ -219,7 +99,7 @@ namespace JokesTutorial.Controllers
             // endpointData.file = fileData;
             endpointData.file = testPayload;
 
-            var data = doPost(apiEndPoint, endpointParams, endpointData);
+            var data = ApiClientTools.Client.doPost(apiEndPoint, endpointParams, endpointData);
             return Json(data.Result);
         }
 
@@ -243,7 +123,7 @@ namespace JokesTutorial.Controllers
             endpointData.client_name = "do post client name";
 
 
-            var data = await doPost(apiEndPoint, endpointParams, endpointData);
+            var data = await ApiClientTools.Client.doPost(apiEndPoint, endpointParams, endpointData);
             return Json(data);
 
             apiEndPoint = "api/users/getAuthData/{id}";
